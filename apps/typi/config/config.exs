c3ab196c7f19c5530195e3b0f198d7a62731e74b
Almas Akchabayev <almas.akchabayev@gmail.com>
2016-05-25 12:10:51 +0600

    changed token exspiration to 6 months

--- apps/typi/config/config.exs
+++ apps/typi/config/config.exs
@@ -31,7 +31,7 @@ config :guardian, Guardian,
   allowed_algos: ["Ed448"],
   issuer: "Typi",
   verify_issuer: true,
-  ttl: { 30, :days },
+  ttl: { 6, :months },
   secret_key: %{
     "alg" => "Ed448",
     "crv" => "Ed448",
