97efaaf567f8ba55e5a5ac9ad02c0771d618bd0d
Almas Akchabayev <almas.akchabayev@gmail.com>
2016-06-27 19:02:46 +0600

    moved status functionality to user_channel

--- apps/typi/web/channels/user_channel.ex
+++ apps/typi/web/channels/user_channel.ex
@@ -1,5 +1,7 @@
 defmodule Typi.UserChannel do
   use Typi.Web, :channel
+  use Amnesia
+  use Typi.Database
 
   def join("users:" <> user_id, _payload, socket) do
     if authorized?(user_id, socket) do
@@ -15,10 +17,18 @@ defmodule Typi.UserChannel do
     {:noreply, socket}
   end
 
+  def handle_in("statuses", %{"statuses" => statuses}, socket) do
+    IO.inspect statuses
+    for status <- statuses do
+      handle_in("status", status, socket)
+    end
+    {:noreply, socket}
+  end
+
   def handle_in("status", %{"id" => message_id, "status" => status} = payload, socket) do
     IO.inspect payload
-    statuses = Typi.ChatChannel.update_status_and_get_statuses(message_id, status, socket)
-    Typi.ChatChannel.broadcast_if_status_changed(statuses, message_id)
+    statuses = update_status_and_get_statuses(message_id, status, socket)
+    broadcast_if_status_changed(statuses, message_id)
     {:noreply, socket}
   end
 
@@ -35,6 +45,51 @@ defmodule Typi.UserChannel do
   def handle_out("message", payload, socket) do
     push socket, "message", payload
     {:noreply, socket}
+  end
+
+  def broadcast_if_status_changed(statuses, message_id) do
+    message = Amnesia.transaction do
+      Message.read(message_id)
+    end
+    current_status = min_status(statuses)
+    if message.status != current_status do
+      message = Amnesia.transaction do
+        Message.read(message_id)
+        |> Map.put(:status, current_status)
+        |> Message.write
+      end
+      Typi.Endpoint.broadcast "users:#{message.user_id}", "message:status", Map.take(message, [:id, :status])
+    end
+  end
+
+  def update_status_and_get_statuses(m_id, status, socket) do
+    Amnesia.transaction do
+      selection = Status.where message_id == m_id and recipient_id == socket.assigns.current_user.id, select: [id]
+      [[status_id]] = selection |> Amnesia.Selection.values
+
+      status_id
+      |> Status.read
+      |> Map.put(:status, status)
+      |> Status.write
+
+      Status.read_at(m_id, :message_id)
+    end
+  end
+
+  defp min_status(statuses) do
+    min_status(statuses, "")
+  end
+
+  defp min_status([], acc) do
+    acc
+  end
+
+  defp min_status([h | t], acc) do
+    cond do
+      h.status == "sending" -> "sending"
+      h.status == "received" or acc == "received" -> min_status(t, "received")
+      true -> min_status(t, "read") # h.status == "read" && acc == "read"
+    end
   end
 
   # Add authorization logic here as required.
